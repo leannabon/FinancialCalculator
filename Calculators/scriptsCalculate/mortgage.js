70eddf40ed54c1df282ed3a3faab3685786ecb62
leannabonalba <80809177+leannabonalba@users.noreply.github.com>
2023-04-28 15:32:59 -0700

    Completed Mortgage Calculator

--- Calculators/scriptsCalculate/mortgage.js
+++ Calculators/scriptsCalculate/mortgage.js
@@ -14,21 +14,24 @@ function init() {
   //Inputs define
   function onCalculateBtnClicked() {
     
-    let principalAmount = Number(document.getElementById("principalAmount").value);
-    let percentAmount = Number(document.getElementById("percentAmount").value);
-    let yearsAmount = Number(document.getElementById("yearsAmount").value);
+    let principal = Number(document.getElementById("principalAmount").value);
+    let percent = Number(document.getElementById("percentAmount").value);
+    let years = Number(document.getElementById("yearsAmount").value);
 
   //Compound interest calculator
-  const result3 = principalAmount * percentAmount;
-  
+    let nomialInterest = percent / 100;
+    let result3 = principal * (Math.pow((1 + nomialInterest / 12),(12 * years)));
+    let result1 = result3 / years;
+    let result2 = result3 - principal;
+
   //Define formula for displaying monthly Payment
-    document.getElementById("monthlyPayField").value = result1;
+    document.getElementById("monthlyPayField").value = result1.toFixed(2);
   
   //Define formula for interest paid
-    document.getElementById("interestPaidField").value = result2;
+    document.getElementById("interestPaidField").value = result2.toFixed(2);
   
   //Define formula for displaying total accrued amount
-    document.getElementById("accruedAmountField").value = result3;
+    document.getElementById("accruedAmountField").value = result3.toFixed(2);
 
 }
 
