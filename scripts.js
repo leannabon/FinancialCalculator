65962afa0e5835f9554d965edbeae9a5fb69d2d9
leannabonalba <80809177+leannabonalba@users.noreply.github.com>
2023-04-28 14:26:52 -0700

    Moved js for mortgage calculator, new folder/file

--- scripts.js
+++ scripts.js
@@ -60,35 +60,3 @@ function init() {
   
 }
 
-//MORTGAGE CALCULATOR
-
-//Define event for button action
-function init() {
-  const calculateBtn = document.getElementById("btnCalculate");
-  calculateBtn.onclick = onCalculateBtnClicked;
-}
-
-//Inputs define
-function onCalculateBtnClicked() {
-  let principalAmount = Number(document.getElementById("principalAmount").value);
-  let percentAmount = Number(document.getElementById("percentAmount").value);
-  let yearsAmount = Number(document.getElementById("yearsAmount").value);
-
-//Compound interest calculator
-
-    let nomialInterest = percentAmount / 100;
-    let accruedAmountField = principalAmount (1 + nomialInterest / 12) ^ 12 * yearsAmount;
-
-
-//Define formula for displaying monthly Payment
-    document.getElementById("monthlyPayField").value = monthlyPayField;
-
-//Define formula for interest paid
-    document.getElementById("interestPaidField").value = interestPaidField;
-
-//Define formula for displaying total accrued amount
-document.getElementById("accruedAmountField").value = accruedAmountField;
-
-
-}
-
